b913fcea655e9bbdbb717b28a8d67b259cc8da79
simon <simon011130@gmail.com>
2024-03-08 09:49:30 +0000

    update eval scripts

--- eval_model.sh
+++ eval_model.sh
@@ -43,8 +43,10 @@ fi
 for BATCH_SIZE_PER_GPU in \
     8 \
     ; do
-    for TASK in ARC gsm8k truthfulqa triviaqa hellaswag; do
-    # for TASK in hellaswag; do
+    # for TASK in ARC gsm8k truthfulqa triviaqa hellaswag; do
+    # for TASK in truthfulqa triviaqa hellaswag; do
+    for TASK in ARC gsm8k truthfulqa triviaqa; do    
+# for TASK in hellaswag; do
     # for TASK in ARC; do
         if [[ $TASK == "ARC" ]]; then
             TASK_LIST="arc_challenge"
