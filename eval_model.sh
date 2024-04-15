89803d358f5d98f84b9cf5f176bbd45fd720a1c3
simonucl <simon011130@gmail.com>
2024-04-15 14:02:14 +0100

    update for the correct tasks

--- eval_model.sh
+++ eval_model.sh
@@ -28,7 +28,7 @@ for BATCH_SIZE_PER_GPU in \
             FEW_SHOT=10
             LIMIT=2000
         elif [[ $TASK == "truthfulqa" ]]; then
-            TASK_LIST="truthfulqa"
+            TASK_LIST="truthfulqa_mc2"
             FEW_SHOT=0
         elif [[ $TASK == "mmlu" ]]; then
             TASK_LIST="hendrycksTest-abstract_algebra,hendrycksTest-anatomy,hendrycksTest-astronomy,hendrycksTest-business_ethics,hendrycksTest-clinical_knowledge,hendrycksTest-college_biology,hendrycksTest-college_chemistry,hendrycksTest-college_computer_science,hendrycksTest-college_mathematics,hendrycksTest-college_medicine,hendrycksTest-college_physics,hendrycksTest-computer_security,hendrycksTest-conceptual_physics,hendrycksTest-econometrics,hendrycksTest-electrical_engineering,hendrycksTest-elementary_mathematics,hendrycksTest-formal_logic,hendrycksTest-global_facts,hendrycksTest-high_school_biology,hendrycksTest-high_school_chemistry,hendrycksTest-high_school_computer_science,hendrycksTest-high_school_european_history,hendrycksTest-high_school_geography,hendrycksTest-high_school_government_and_politics,hendrycksTest-high_school_macroeconomics,hendrycksTest-high_school_mathematics,hendrycksTest-high_school_microeconomics,hendrycksTest-high_school_physics,hendrycksTest-high_school_psychology,hendrycksTest-high_school_statistics,hendrycksTest-high_school_us_history,hendrycksTest-high_school_world_history,hendrycksTest-human_aging,hendrycksTest-human_sexuality,hendrycksTest-international_law,hendrycksTest-jurisprudence,hendrycksTest-logical_fallacies,hendrycksTest-machine_learning,hendrycksTest-management,hendrycksTest-marketing,hendrycksTest-medical_genetics,hendrycksTest-miscellaneous,hendrycksTest-moral_disputes,hendrycksTest-moral_scenarios,hendrycksTest-nutrition,hendrycksTest-philosophy,hendrycksTest-prehistory,hendrycksTest-professional_accounting,hendrycksTest-professional_law,hendrycksTest-professional_medicine,hendrycksTest-professional_psychology,hendrycksTest-public_relations,hendrycksTest-security_studies,hendrycksTest-sociology,hendrycksTest-us_foreign_policy,hendrycksTest-virology,hendrycksTest-world_religions"
@@ -41,7 +41,7 @@ for BATCH_SIZE_PER_GPU in \
             FEW_SHOT=3
 	        LIMIT=500
         elif [[ $TASK == "gsm8k" ]]; then
-            TASK_LIST="gsm8k"
+            TASK_LIST="gsm8k-cot"
             FEW_SHOT=5
         elif [[ $TASK == "triviaqa" ]]; then
             TASK_LIST="triviaqa"
