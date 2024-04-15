1d6e8b2d684fdd82decc5d4499c7953b11ab1ab0
simonucl <simon011130@gmail.com>
2024-04-15 12:45:10 +0100

    eval

--- eval_model.sh
+++ eval_model.sh
@@ -28,7 +28,7 @@ for BATCH_SIZE_PER_GPU in \
             FEW_SHOT=10
             LIMIT=2000
         elif [[ $TASK == "truthfulqa" ]]; then
-            TASK_LIST="truthfulqa_mc"
+            TASK_LIST="truthfulqa"
             FEW_SHOT=0
         elif [[ $TASK == "mmlu" ]]; then
             TASK_LIST="hendrycksTest-abstract_algebra,hendrycksTest-anatomy,hendrycksTest-astronomy,hendrycksTest-business_ethics,hendrycksTest-clinical_knowledge,hendrycksTest-college_biology,hendrycksTest-college_chemistry,hendrycksTest-college_computer_science,hendrycksTest-college_mathematics,hendrycksTest-college_medicine,hendrycksTest-college_physics,hendrycksTest-computer_security,hendrycksTest-conceptual_physics,hendrycksTest-econometrics,hendrycksTest-electrical_engineering,hendrycksTest-elementary_mathematics,hendrycksTest-formal_logic,hendrycksTest-global_facts,hendrycksTest-high_school_biology,hendrycksTest-high_school_chemistry,hendrycksTest-high_school_computer_science,hendrycksTest-high_school_european_history,hendrycksTest-high_school_geography,hendrycksTest-high_school_government_and_politics,hendrycksTest-high_school_macroeconomics,hendrycksTest-high_school_mathematics,hendrycksTest-high_school_microeconomics,hendrycksTest-high_school_physics,hendrycksTest-high_school_psychology,hendrycksTest-high_school_statistics,hendrycksTest-high_school_us_history,hendrycksTest-high_school_world_history,hendrycksTest-human_aging,hendrycksTest-human_sexuality,hendrycksTest-international_law,hendrycksTest-jurisprudence,hendrycksTest-logical_fallacies,hendrycksTest-machine_learning,hendrycksTest-management,hendrycksTest-marketing,hendrycksTest-medical_genetics,hendrycksTest-miscellaneous,hendrycksTest-moral_disputes,hendrycksTest-moral_scenarios,hendrycksTest-nutrition,hendrycksTest-philosophy,hendrycksTest-prehistory,hendrycksTest-professional_accounting,hendrycksTest-professional_law,hendrycksTest-professional_medicine,hendrycksTest-professional_psychology,hendrycksTest-public_relations,hendrycksTest-security_studies,hendrycksTest-sociology,hendrycksTest-us_foreign_policy,hendrycksTest-virology,hendrycksTest-world_religions"
@@ -55,14 +55,25 @@ for BATCH_SIZE_PER_GPU in \
         echo "TASK: $TASK"
         echo "BATCH_SIZE_PER_GPU: $BATCH_SIZE_PER_GPU"
 
-	if [[ $TASK == "math" ]] || [[ $TASK == "hellaswag" ]]; then
+	if [[ $TASK == "gsm8k" ]]; then
+        lm_eval --model vllm \
+            --model_args="pretrained=${MODEL_NAME_OR_PATH},tensor_parallel_size=2,dtype=auto,gpu_memory_utilization=0.8,data_parallel_size=1" \
+            --tasks=$TASK_LIST \
+            --batch_size auto 
+    elif [[ $TASK == "math" ]]; then
 	    lm_eval --model vllm \
 		    --model_args="pretrained=${MODEL_NAME_OR_PATH},tensor_parallel_size=2,dtype=auto,gpu_memory_utilization=0.8,data_parallel_size=1" \
 		    --tasks=$TASK_LIST \
 		    --batch_size auto \
 		    --limit=$LIMIT
-    	else
-	    lm_eval --model vllm \
+    elif [[ $TASK == "hellaswag" ]]; then
+        lm_eval --model hf \
+            --model_args="pretrained=${MODEL_NAME_OR_PATH},tensor_parallel_size=2,dtype=auto,gpu_memory_utilization=0.8,data_parallel_size=1" \
+            --tasks=$TASK_LIST \
+            --batch_size auto \
+            --limit=$LIMIT
+    else
+	    lm_eval --model hf \
 		    --model_args="pretrained=${MODEL_NAME_OR_PATH},tensor_parallel_size=2,dtype=auto,gpu_memory_utilization=0.8,data_parallel_size=1" \
 		    --tasks=$TASK_LIST \
 		    --batch_size auto
