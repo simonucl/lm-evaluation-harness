b36bc0178471c31e073878230da2a1d96d9559dc
simon <simon011130@gmail.com>
2024-05-20 16:38:30 +0000

    add seqit

--- eval_model_seqit.sh
+++ eval_model_seqit.sh
@@ -1,11 +1,26 @@
-for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-flancot_full_sit_llama_70b_iter_2
+# for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-flancot_full_sit_llama_70b_iter_2
+# for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-alpaca_it_llmam_70b simonycl/self-seq-Meta-Llama-3-8B-alpaca_sit_llama_70b simonycl/self-seq-Meta-Llama-3-8B-alpaca_sit_llama_70b
+for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-wizardlm
 do
     accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=${CHECKPOINT_PATH},attn_implementation=flash_attention_2,use_chat_template=True \
+    --model_args pretrained=${CHECKPOINT_PATH},attn_implementation=flash_attention_2,use_chat_template=True	\
     --gen_kwargs temperature=0,top_k=0,top_p=0 \
-    --tasks mmlu_flan_cot_zeroshot \
+    --tasks mmlu \
+    --num_fewshot 5 \
     --batch_size auto \
-    --output_path results/mmlu/${CHECKPOINT_PATH}_flan_cot_zeroshot \
-    --use_cache db/mmlu_flan_cot_zeroshot.db \
-    --cache_requests true
+    --output_path results/mmlu/${CHECKPOINT_PATH}
+
+    # lm_eval --model vllm \
+    # --model_args pretrained=${CHECKPOINT_PATH},tensor_parallel_size=2,dtype=auto,gpu_memory_utilization=0.85,use_chat_template=True     \
+    # --tasks mmlu_flan_cot_zeroshot \
+    # --batch_size 2 \
+    # --output_path results/mmlu/${CHECKPOINT_PATH}_flan_cot_zeroshot
+
+    accelerate launch -m lm_eval --model hf \
+    --model_args pretrained=${CHECKPOINT_PATH},attn_implementation=flash_attention_2,use_chat_template=True     \
+    --gen_kwargs temperature=0,top_k=0,top_p=0 \
+    --tasks arc_challenge \
+    --num_fewshot 25 \
+    --batch_size auto \
+    --output_path results/arc/${CHECKPOINT_PATH}
 done
