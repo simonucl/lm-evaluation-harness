842c30b76bdb823b04374d92c7175db414fdd47a
simonucl <simon011130@gmail.com>
2024-05-20 17:46:20 +0100

    add new scripts

--- eval_model_seqit.sh
+++ eval_model_seqit.sh
@@ -1,6 +1,6 @@
 # for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-flancot_full_sit_llama_70b_iter_2
 # for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-alpaca_it_llmam_70b simonycl/self-seq-Meta-Llama-3-8B-alpaca_sit_llama_70b simonycl/self-seq-Meta-Llama-3-8B-alpaca_sit_llama_70b
-for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-wizardlm
+for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-flancot_full_it_llama_70b simonycl/self-seq-Meta-Llama-3-8B-flancot_full_sit_llama_70b_iter_2 simonycl/self-seq-Meta-Llama-3-8B-alpaca_it_llmam_70b simonycl/self-seq-Meta-Llama-3-8B-alpaca_llmam_70b-iter-2
 do
     accelerate launch -m lm_eval --model hf \
     --model_args pretrained=${CHECKPOINT_PATH},attn_implementation=flash_attention_2,use_chat_template=True	\
@@ -10,11 +10,17 @@ do
     --batch_size auto \
     --output_path results/mmlu/${CHECKPOINT_PATH}
 
-    # lm_eval --model vllm \
-    # --model_args pretrained=${CHECKPOINT_PATH},tensor_parallel_size=2,dtype=auto,gpu_memory_utilization=0.85,use_chat_template=True     \
-    # --tasks mmlu_flan_cot_zeroshot \
-    # --batch_size 2 \
-    # --output_path results/mmlu/${CHECKPOINT_PATH}_flan_cot_zeroshot
+done
+
+for CHECKPOINT_PATH in simonycl/self-seq-Meta-Llama-3-8B-wizardlm simonycl/self-seq-Meta-Llama-3-8B-flancot_full_it_llama_70b simonycl/self-seq-Meta-Llama-3-8B-flancot_full_sit_llama_70b_iter_2 simonycl/self-seq-Meta-Llama-3-8B-alpaca_it_llmam_70b simonycl/self-seq-Meta-Llama-3-8B-alpaca_llmam_70b-iter-2
+do
+        accelerate launch -m lm_eval --model hf \
+    --model_args pretrained=${CHECKPOINT_PATH},attn_implementation=flash_attention_2,use_chat_template=True	\
+    --gen_kwargs temperature=0,top_k=0,top_p=0 \
+    --tasks mmlu \
+    --num_fewshot 0 \
+    --batch_size auto \
+    --output_path results/mmlu/${CHECKPOINT_PATH}_0shot
 
     accelerate launch -m lm_eval --model hf \
     --model_args pretrained=${CHECKPOINT_PATH},attn_implementation=flash_attention_2,use_chat_template=True     \
@@ -23,4 +29,5 @@ do
     --num_fewshot 25 \
     --batch_size auto \
     --output_path results/arc/${CHECKPOINT_PATH}
+
 done
